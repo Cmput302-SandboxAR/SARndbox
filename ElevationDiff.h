9ef2e9d393e4bee459944525d143b46f2e45d091
Joel Johnston <joel@uws100677usr.uws.ualberta.ca>
2016-03-10 15:12:37 -0700

    Made subtraction work.

--- ElevationDiff.h
+++ ElevationDiff.h
@@ -4,7 +4,15 @@
 //  * @brief Subtracts values in frame buff b from frame buff a
 //  * @return a frame buffer containing the difference
 //  **
-Kinect::FrameBuffer BufferDifference(Kinect::FrameBuffer a, Kinect::FrameBuffer b)
+void BufferDifference(Kinect::FrameBuffer a, Kinect::FrameBuffer b)
 {
+    
+    float* diPtrA=static_cast<float*>(a.getBuffer());
+    float* diPtrB=static_cast<float*>(b.getBuffer());
+
+    for(unsigned int y=0;y<a.getSize(1);++y)
+        for(unsigned int x=0;x<a.getSize(0);++x,++diPtrA,++diPtrB)
+            *diPtrA -= *diPtrB;
+    
     
 }
